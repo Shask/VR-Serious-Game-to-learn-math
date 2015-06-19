721c5d7a02375e0be5e903ad181a60f788837551
yannismrad <yannismrad@free.fr>
2015-06-19 19:54:30 -0400

    Scene attraper cube + lacher
    + dossier materials
    + dossier imported assets (pour assets externes, trucs du store)
    + dossier gameplay dans scripts
    +2 textures

---
+++ Mathaptique/Assets/Script/Gameplay/ObjectDetection.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectDetection : MonoBehaviour {
+    GameObject player;
+    private int nbCollisions, maxCollisions;
+    private string interactiveTag = "Interactive";
+
+	// Use this for initialization
+	void Start () {
+        player = GameObject.Find("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == interactiveTag)
+        {
+            nbCollisions++;
+            if (!player.GetComponent<Player>().getIsGrabbingItem())
+            {
+                player.GetComponent<Player>().setcanGrabItem(true);
+                player.GetComponent<Player>().setItemInRange(col.gameObject);
+                col.gameObject.GetComponent<Renderer>().material.color = new Color(255, 0, 0);
+                Debug.Log("can grab item");
+            }
+               
+        }
+        
+    }
+
+    void OnTriggerExit(Collider col)
+    {
+        if (col.gameObject.tag == interactiveTag)
+        {
+            nbCollisions--;
+            player.GetComponent<Player>().setcanGrabItem(false);
+            player.GetComponent<Player>().setItemInRange(null);
+            col.gameObject.GetComponent<Renderer>().material.color = new Color(255, 255, 255);
+            Debug.Log("can't grab item anymore");
+        }
+        
+    }
+}
